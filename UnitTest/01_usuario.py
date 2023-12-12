4384e6e3a53ce0f3ce303f09404edafaf088d673
Mariano Barraco <marianobarraco@gmail.com>
2023-12-12 20:53:20 -0300

    clase 24

--- UnitTest/01_usuario.py
+++ UnitTest/01_usuario.py
@@ -1,11 +1,34 @@
-
-
 # Implementar una clase Usuario que tenga:
 # Metodos: obtener_nombre(), obtener_email(), es_mayor_de_edad(), edad(),
 
-
 class Usuario:
-    pass
+
+    def __init__(self, nombre, email="indefinido"):
+        self.nombre = nombre
+        self.email = email
+
+    def obtener_nombre(self):
+        return self.nombre
+
+    def obtener_email(self):
+        return self.email
+
+
+user = Usuario("Nicolas", "nicol@s.com")
+email = user.obtener_email()
+
+if email == "nicol@s.com":
+    print("Prueba exitosa")
+else:
+    print("Prueba fallida")
+
+
+user = Usuario("XXXXXXXXXXX")
+email = user.obtener_email()
+if email == "indefinido":
+    print("Prueba exitosa")
+else:
+    print("Prueba fallida")
 
 
 user = Usuario("Olivia")
---
+++ UnitTest/02_calculadora.py
@@ -0,0 +1,19 @@
+
+
+def sumar(a, b):
+    return a + b
+
+
+resultado = sumar(10, 9)
+if resultado == 19:
+    print("Prueba exitosa")
+else:
+    print("Prueba fallida")
+
+
+
+resultado = sumar(-1, 9)
+if resultado == 8:
+    print("Prueba exitosa")
+else:
+    print("Prueba fallida")
